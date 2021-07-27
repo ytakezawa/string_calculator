3f6d2277183afc5261febe526be9b314b20648a5
Yoshika Takezawa <ytakezaw@stevens.edu>
2021-07-27 16:34:45 +0100

    new test

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -11,3 +11,15 @@ def test_correct_params_expect_OP_NUM_NUM():
     
     # Assert
     assert result == expResult
+
+def test_correct_params_expect_INVALID_OP_NUM_NUM():
+    # Arrange
+    cut = string_calculator.StringCalculator()
+    inputVal = "A, 3, 4"
+    expResult = ["INVALID_OPERATOR", "NUMBER", "NUMBER"]
+    
+    # Act
+    result = cut.parse(inputVal)
+    
+    # Assert
+    assert result == expResult
